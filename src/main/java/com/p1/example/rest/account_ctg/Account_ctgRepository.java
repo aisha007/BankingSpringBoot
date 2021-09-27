1108f9481c91a42b721d40768f0029b25043fb22
Anisha Fernandes <anisharose.1502@gmail.com>
2021-09-27 09:30:51 -0700

    Changes in Account_ctg

---
+++ src/main/java/com/p1/example/rest/account_ctg/Account_ctgRepository.java
@@ -0,0 +1,10 @@
+package com.p1.example.rest.account_ctg;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.p1.example.rest.account_ctg.Account_ctg;;
+
+public interface Account_ctgRepository extends JpaRepository<Account_ctg, String>
+{
+	
+}
